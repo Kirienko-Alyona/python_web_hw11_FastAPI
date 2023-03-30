14a5404540945af15ae01b07a27e45487014c246
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-03-31 00:52:19 +0300

    changes

--- hw11/hw11/src/repository/contacts.py
+++ hw11/hw11/src/repository/contacts.py
@@ -6,7 +6,9 @@ from src.schemas.contacts import ContactModel, ContactUpdate
 from src.database.models import Contact
 
 
-async def get_contacts(search_name: str, search_surname: str, search_email: str, search_phone: str, limit: int, offset: int, db: Session) -> Optional[List[Contact]]:
+async def get_contacts_search(search_name: str, search_surname: str, search_email: str, search_phone: str, limit: int, offset: int, db: Session) -> Optional[List[Contact]]:
+    #if not input params - returned all list contacts 
+    #else - search by parametrs: name, surname, email, phone - returned list contacts     
     contacts = db.query(Contact)
     if search_name:
         #contacts = contacts.filter(Contact.name.ilike(f'%{s_name}%')) - робить те ж саме, що і icontains
@@ -17,14 +19,16 @@ async def get_contacts(search_name: str, search_surname: str, search_email: str,
         contacts = contacts.filter(Contact.email.icontains(search_email))   
     if search_phone:
         contacts = contacts.filter(Contact.phone.icontains(search_phone)) 
-    contacts = contacts.limit(limit).offset(offset).all()
+    contacts = contacts.limit(limit).offset(offset).all()   
     return contacts
 
-async def get_contact(contact_id: int, db: Session) -> Contact:
+async def get_contact_id(contact_id: int, db: Session) -> Contact:
+    #search one contact by contact id - return only one contact
     contact = db.query(Contact).filter_by(id=contact_id).first()
     return contact
 
 async def create_contact(body: ContactModel, db: Session) -> Contact:
+    #create contact
     contact = Contact(**body.dict())
     db.add(contact)
     db.commit()
@@ -32,6 +36,7 @@ async def create_contact(body: ContactModel, db: Session) -> Contact:
     return contact
 
 async def update_contact(body: ContactUpdate, contact_id: int, db: Session) -> Contact| None:
+    #update contact
     contact = db.query(Contact).filter_by(id=contact_id).first()
     if contact:
         contact.name = body.name,
@@ -43,15 +48,9 @@ async def update_contact(body: ContactUpdate, contact_id: int, db: Session) -> C
     return contact    
 
 async def remove_contact(contact_id: int, db: Session) -> Contact| None:
+    #delete contact
     contact = db.query(Contact).filter_by(id=contact_id).first()
     if contact:
         db.delete(contact)
         db.commit()
     return contact    
-
-async def search_by_name(search_name: str, db: Session):# -> Optional[Contact]:
-    
-    contact = db.query(Contact).filter_by(name=search_name).first()
-    """To search for a record by a specific name."""
-    # return db.query(Contact).filter(Contact.name == name).first()  # .all()
-    return contact
