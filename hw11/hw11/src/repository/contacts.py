b46a415feb08c497e201d42f8c1b2b063727885a
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-04-01 02:31:46 +0300

    finish

--- hw11/hw11/src/repository/contacts.py
+++ hw11/hw11/src/repository/contacts.py
@@ -2,7 +2,6 @@ from datetime import date, timedelta
 from typing import List, Optional
 
 from sqlalchemy.orm import Session
-from sqlalchemy import extract
 
 from src.schemas.contacts import ContactModel, ContactUpdate
 from src.database.models import Contact
