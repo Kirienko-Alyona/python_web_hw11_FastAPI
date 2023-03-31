1e8c7c19f1be91a477560cf4638a7db4aceda576
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-03-31 05:08:18 +0300

    del comment

--- hw11/hw11/src/database/models.py
+++ hw11/hw11/src/database/models.py
@@ -13,17 +13,3 @@ class Contact(Base):
     email = Column(String, unique=True, index=True)
     phone = Column(String, unique=True, nullable=False)
     born_date = Column(Date)   
-    
-    
-    # def __repr__(self):
-    #     return "<name={}, surname={}, email={}, phone={}, born_date={}>".format(self.name, self.surname, self.email, self.phone, self.born_date)
-
-    # @property
-    # def serialize(self):
-    #     return {
-    #         'name': self.name,
-    #         'surname': self.surname,
-    #         'email': self.email,
-    #         'phone': self.phone,
-    #         'born_date': self.born_date,
-    #     }
